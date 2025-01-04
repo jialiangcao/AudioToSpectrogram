aa8355854b4732c84b38e360a6d3ca7d55f12c71
jialiangcao <jcao2080@gmail.com>
2025-01-03 20:07:59 -0500

    Remove print log

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ import struct
 
 app = Flask(__name__)
 
-# Constants
 SR = 44100
 AUDIO_N_MELS = 64
 N_FFT = 1024
@@ -16,21 +15,14 @@ HOP_LENGTH = N_FFT // 4
 @app.route('/process_audio', methods=['POST'])
 def process_audio():
     try:
-        # debug
-        print("Received POST request to /process_audio")
         compressedAudioData = request.get_data()
-        print(f"Received compressed binary audio data")
         
-        print(f"Length of compressed data: {len(compressedAudioData)}")
         with gzip.GzipFile(fileobj=io.BytesIO(compressedAudioData)) as f:
             audioData = f.read()
-            print(f"Length of uncompressed data: {len(audioData)}")
 
-        doubleCount = len(audioData) // 8 # 8 for 64 bit double values
+        doubleCount = len(audioData) // 8
         audioData = struct.unpack(f'{doubleCount}d', audioData)
         waveform = torch.tensor(list(audioData), dtype=torch.float32).unsqueeze(0)
-        # using float32 may be incorrect
-        print(f"Shape of waveform tensor: {waveform.shape}")
 
         mel_spec = torchaudio.transforms.MelSpectrogram(
             sample_rate=SR,
@@ -39,16 +31,11 @@ def process_audio():
             hop_length=HOP_LENGTH,
         )(waveform)
 
-        print(f"Shape of mel spectrogram: {mel_spec.shape}")
-
         mel_db = torchaudio.transforms.AmplitudeToDB()(mel_spec)
-        print(f"Shape of mel_db: {mel_db.shape}")
         
         mel_normalized = (mel_db - mel_db.mean()) / (mel_db.std() + 1e-6)
-        print(f"Shape of normalized mel spectrogram: {mel_normalized.shape}")
         
         response = jsonify({"mel_spectrogram": mel_normalized.squeeze().tolist()})
-        print("Sending response")
         return response, 200
     except Exception as e:
         print(f"Error occurred: {str(e)}")
