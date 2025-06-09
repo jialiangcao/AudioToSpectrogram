f672af8aff42eb33dd2a5d28f4c023579b5f6059
Jialiang Cao <jcao2080@gmail.com>
2025-06-09 11:30:19 -0400

    Add Cloud API support

--- main.py
+++ main.py
@@ -3,14 +3,20 @@ import torch
 import torchaudio
 import struct
 import numpy
+import os
 
 from functools import wraps
 import firebase_admin
 from firebase_admin import credentials, auth
 
 # Firebase
-cred = credentials.Certificate('serviceAccountKey.json')
-firebase_admin.initialize_app(cred)
+if not firebase_admin._apps:
+    if os.getenv("GOOGLE_CLOUD_PROJECT"):
+        cred = credentials.ApplicationDefault()
+    else:
+        cred = credentials.Certificate('serviceAccountKey.json')
+    firebase_admin.initialize_app(cred)
+
 def firebase_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
